6d690736a6f2b753e390168357c8baaf0de77fc4
c1401892 <samir.santos@bematech.com.br>
2017-10-06 15:19:44 -0300

    Exemplos do Cap 21x Ajustes Gradle

---
+++ src/main/kotlin/br/com/samirrolemberg/c2xx/c211.kt
@@ -0,0 +1,6 @@
+package br.com.samirrolemberg.c2xx
+
+//tipo é descrito depois da variavel na assinatura do metodo
+fun main(args: Array<String>) {//Array de Kotlin = Java[]
+    println("Hi! <3")//muitas coisas de Lang estão em Wrapepr dentro do Kotlin
+}
